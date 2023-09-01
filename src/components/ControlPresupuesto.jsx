29ab7a0066feabbef513e82c1bf45673d562e71b
julian95844 <julian95844@hotmail.com>
2023-09-01 02:32:53 -0300

    Calculando presupuesto gastado y disponible, añadiendo swipe

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,22 @@
-const ControlPresupuesto = ({presupuesto}) => {
+import { useEffect, useState } from "react"
+
+
+const ControlPresupuesto = ({gastos, presupuesto}) => {
+
+    const [disponible, setDisponible] = useState (0)
+    const [gastado, setGastado] = useState (0)
+
+    useEffect(() => {
+      const totalGastado = gastos.reduce((total, gasto) => total + parseFloat(gasto.cantidad), 0);
+      
+      const totalDisponible = presupuesto - totalGastado
+
+      setDisponible(totalDisponible)
+
+      setGastado(totalGastado)
+
+      
+    }, [gastos]);
 
     const formatearCantidad = (cantidad) => {
         return cantidad.toLocaleString("en-US" , {
@@ -18,11 +36,11 @@ const ControlPresupuesto = ({presupuesto}) => {
         </p>
 
         <p>
-            <span>Disponible: </span> {formatearCantidad(0)}
+            <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
 
         <p>
-            <span>Gastos: </span> {formatearCantidad(0)}
+            <span>Gastos: </span> {formatearCantidad(gastado)}
         </p>
       </div>
       
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions
+} from "react-swipeable-list"
+import "react-swipeable-list/dist/styles.css"
 import { formatearFecha } from '../helper';
 
 import IconoAhorro from "../img/icono_ahorro.svg"
@@ -21,29 +29,52 @@ const diccionarioIconos = {
 
 const Gasto = ({gasto}) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
+
+  const trailingActions = () =>(
+    <TrailingActions>
+      <SwipeAction onClick={() => console.log("eliminar")}>
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  )
+
+  const leadingActions = () =>(
+    <LeadingActions>
+      <SwipeAction onClick={() => console.log("editando")}>
+        Editar
+      </SwipeAction>
+  </LeadingActions>
+  )
   return (
-    <div className='gasto sombra'>
-        <div className='contenido-gasto'>
-           {/* imagenes */}
-          <img
-            src={diccionarioIconos[categoria]}
-            alt='icono gastos'
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+          <div className='gasto sombra'>
+              <div className='contenido-gasto'>
+                {/* imagenes */}
+                <img
+                  src={diccionarioIconos[categoria]}
+                  alt='icono gastos'
 
-          />
+                />
 
-            <div className='descripcion-gasto'>
+                  <div className='descripcion-gasto'>
 
-                <p className='categoria'>{categoria}</p>
-                <p className='nombre-gasto'>{nombre}</p>
-                <p className='fecha-gasto'>
-                  Agregado el: {""}
-                  <span>{formatearFecha(fecha)}</span>
-                </p>
-            </div>
-        </div>
+                      <p className='categoria'>{categoria}</p>
+                      <p className='nombre-gasto'>{nombre}</p>
+                      <p className='fecha-gasto'>
+                        Agregado el: {""}
+                        <span>{formatearFecha(fecha)}</span>
+                      </p>
+                  </div>
+              </div>
 
-        <p className='cantidad-gasto'>${cantidad}</p>
-    </div>
+              <p className='cantidad-gasto'>${cantidad}</p>
+          </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
